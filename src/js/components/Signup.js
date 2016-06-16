da2978939bc814d30e6ee7d469c1241c62f60d1a
Julian Brendl <julianbrendl@gmail.com>
2016-06-16 02:32:25 +0200

    added editor code

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -1,17 +1,36 @@
 import React from "react"
-import Typist from 'react-typist';
+import Typist from 'react-typist'
 
 var Signup = React.createClass({
 
+    delayGen(mean, std, {line, lineIdx, charIdx, defDelayGenerator}) {
+        if (lineIdx === 4 && charIdx === 1) {
+            return 1000
+        } else if (lineIdx === 5 && charIdx === line.length - 1) {
+            return 1000
+        }
+        return defDelayGenerator(mean - 20)
+    },
+
+    onTypingDone() {
+        var node = React.findDOMNode(this)
+    },
+
     render() {
-        
+
         return (
             <div className="editor">
                 <div className="innerEditor">
-                    <Typist className="typedText">
+                    <Typist className="typedText" cursor={{element: '|', hideWhenDone: true}}
+                            delayGenerator={this.delayGen} onTypingDone={this.onTypingDone}>
                         bot.<span className="blueSyntax">is</span>(<span className="orangeSyntax">"Bro"</span>); 
-                        <span className="greenSyntax">//Bot will talk like a bro</span>
+                        <span className="greenSyntax"> //Bot will talk like a bro</span>
+                        <br/>
+                        bot.<span className="blueSyntax">say</span>(<span className="orangeSyntax">"EarlyAccess"</span>);
                     </Typist>
+                </div>
+                <div className="messenger">
+
                 </div>
             </div>
         )
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./components/Layout";
+
+// import css
 import "../css/index.css"
+import "../css/signup.css"
 
 ReactDOM.render(<Layout/>, document.getElementById('root'));
