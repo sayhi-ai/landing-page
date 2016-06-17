a2d0c7041cc611c0f624a118c1c9166c7b65de61
Rene Brandel <renebrandel@outlook.com>
2016-06-17 16:59:43 +0200

    SVG works

--- src/js/components/pages/CleanCodePage.js
+++ src/js/components/pages/CleanCodePage.js
@@ -1,25 +1,26 @@
 import React from "react"
 import PageContainer from "../PageContainer"
-import InlineSVG from 'svg-inline-react';
 import transition from "../../../resources/img/transition.svg"
 
 export default class CleanCodePage extends React.Component {
-    
+
     render() {
+
+        const style = {
+            height: "100%",
+            width: "10vw"
+        }
         return (
             <PageContainer padding={this.props.padding}>
                 <div class="cleanCodeLeft">
 
                 </div>
                 <div class="cleanCodeMiddle">
-                    <div class="transitionSVG">
-                        <img src={transition}/>
-                    </div>
+                    <i style={style} dangerouslySetInnerHTML={{__html:transition}} />
                 </div>
                 <div class="cleanCodeRight">
 
                 </div>
-            </PageContainer>
-        )
+            </PageContainer>)
     }
 }
