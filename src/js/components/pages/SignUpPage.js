6f8f8a0cc0434be7cfefd4f9e2abdd812a6564ef
Julian Brendl <julianbrendl@gmail.com>
2016-06-18 13:26:10 +0200

    fixed signup page

--- src/js/components/pages/SignUpPage.js
+++ src/js/components/pages/SignUpPage.js
@@ -11,7 +11,6 @@ import "../../../css/signuppage.css"
 export default class Contact extends React.Component {
     render() {
         var styles = {
-            padding: "5%",
             height: "75vh"
         }
         
