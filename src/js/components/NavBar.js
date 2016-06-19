6f249d4c4fa83593750180fee3f7873f97bfd34d
Julian Brendl <julianbrendl@gmail.com>
2016-06-19 12:42:14 +0200

    added encourage navbar animation

--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -3,10 +3,38 @@ import React from "react"
 export default class NavBar extends React.Component {
     constructor() {
         super()
+        
+        this.state = {
+            navbar: ""
+        }
     }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll.bind(this));
+    }
+
+    handleScroll(event) {
+        let scrollTop = event.srcElement.body.scrollTop
+        let itemTranslate = Math.min(0, scrollTop/3 - 60)
+
+        if (itemTranslate == 0) {
+            this.setState({
+                navbar: "opaque-navbar"
+            });
+        } else {
+            this.setState({
+                navbar: ""
+            });
+        }
+    }
+
     render() {
         return (
-            <div className="navbar noselect mui--z1 " id="navbar">
+            <div className={"navbar noselect mui--z1 " + this.state.navbar} id="navbar">
                 <p className="navbar-left navbar-inline">
                     <span id="navbar-logo"></span>
                     <a href="/">enCourage</a>
