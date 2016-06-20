11aae952345162d59a21ea272b74858062cffc5c
Julian Brendl <julianbrendl@gmail.com>
2016-06-20 17:06:06 +0200

    changed layout on hero

--- src/js/components/Hero.js
+++ src/js/components/Hero.js
@@ -9,38 +9,33 @@ import "../../css/hero.css"
 export default class Hero extends React.Component {
     render() {
         var styles = {
-            minHeight: "90vh",
+            minHeight: "100vh",
             height: "auto"
         }
 
         return (
             <PageContainer styles={styles}>
-                <div className="hero-title-div">
-                    <h1 className="hero-title">Give your bot a life</h1>
-                </div>
-                <div className="hero-outter-table">
-                    <div className="hero-outter-table-cell">
-                        <div className="hero-table">
-                            <div className="hero-image-row">
+                <div className="outter">
+                    <div className="inner">
+                        <div className="hero-title-div">
+                            <h1 className="hero-title">Give your bot a life</h1>
+                        </div>                        <div className="upcoming-features">
+                            <div className="hero-box">
                                 <div className="hero-image">
                                     <img src={manual1}/>
-                                </div>
-                                <div className="hero-image">
-                                    <img src={manual2}/>
-                                </div>
-                                <div className="hero-image">
-                                    <img src={manual3}/>
-                                </div>
-                            </div>
-                            <div className="hero-label-row">
-                                <div className="hero-label">
                                     <h4>Give your bot</h4>
                                 </div>
-                                <div className="hero-label">
-                                    <h4>Magic!</h4>
+                            </div>
+                            <div className="hero-box">
+                                <div className="hero-image">
+                                    <img src={manual2}/>
+                                    <h4>Give your bot</h4>
                                 </div>
-                                <div className="hero-label">
-                                    <h4>Get your Bot</h4>
+                            </div>
+                            <div className="hero-box">
+                                <div className="hero-image">
+                                    <img src={manual3}/>
+                                    <h4>Give your bot</h4>
                                 </div>
                             </div>
                         </div>
