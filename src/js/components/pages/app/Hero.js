886055d6ca8ba0a98e699d63c97793cebc4a11ff
Rene Brandel <renebrandel@outlook.com>
2016-06-28 15:55:38 +0200

    Hero Page completed

--- src/js/components/pages/app/Hero.js
+++ src/js/components/pages/app/Hero.js
@@ -24,8 +24,8 @@ export default class Hero extends React.Component {
                 <div className="outter">
                     <div className="inner">
                         <div className="hero-title-div">
-                            <h1 className="hero-title">
-                                <span className="">Human responses</span><br/> for your bot
+                            <h1 className="hero-title accent-color">
+                                Human responses for your bot
                             </h1>
                             <div className="hero-moto-div">
                                 <h4 className="hero-moto">Turn your bot's language into human language</h4>
